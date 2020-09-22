d4b76883c83b0d61601394f2ca7289634237c68b
Ronnie Feng <ronnie@ronniefeng.com>
2020-09-22 19:04:19 -0400

    adding the header to the App, will add contact and sign in later on

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Homepage from "./pages/homepage/homepage.component.jsx"
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
         <div>
+          <Header/>
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route exact path="/shop" component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({id, name, price, imageURL}) =>{
+const CollectionItem = ({id, name, price, imageUrl}) =>{
     return <div className="collection-item">
         <div className="image"
             style={{
-                backgroundImage: `url(${imageURL})`
+                backgroundImage: `url(${imageUrl})`
             }}
         />
         <div className="collection-footer">
