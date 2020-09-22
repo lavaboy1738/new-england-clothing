56ed53587cf8aed0aae5e3c61d62af4fc6da15bc
Ronnie Feng <ronnie@ronniefeng.com>
2020-09-21 22:02:09 -0400

    finishing menu items and styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./mesnu-item.styles.scss";
 
 const MenuItem = ({title, url, size}) => (
-    <div className={`${size} menu-item`}
-        style={{
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={{
             backgroundImage: `url(${url})`
-        }}
-    >
+        }}></div>
         <div className="content">
-            <h1 className="title">{ title}</h1>
+            <h1 className="title">{ title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
