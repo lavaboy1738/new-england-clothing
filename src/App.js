c2f14b09434fc93bfb3e30d70fa66eed3cbef7cf
Ronnie Feng <ronnie@ronniefeng.com>
2020-09-22 23:07:27 -0400

    adding sign in and sign up page, input form component, and custom button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Homepage from "./pages/homepage/homepage.component.jsx"
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route exact path="/shop" component={ShopPage} />
+            <Route exact path="/signin" component={SignInAndSignUpPage} />
           </Switch>
         </div>
   );
---
+++ src/components/form-input/form-input.components.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './form-input.styles.scss';
+
+const FormInput = ({handleChange, label, ...otherProps}) =>{
+    return <div className="group">
+        <input type="text" className="form-input" onChange={handleChange} {...otherProps} />
+        {
+            label ? 
+            (<label className={`${otherProps.value.length? "shrink" : ""} form-input-label`}>
+                {label}
+            </label>)
+            : null
+        }
+    </div>
+}
+
+export default FormInput;
