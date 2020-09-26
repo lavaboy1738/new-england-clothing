8b607ef64041fc89767ed7ac9f5bdc12b28f0263
Ronnie Feng <ronnie@ronniefeng.com>
2020-09-26 16:26:39 -0400

    initializing redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
         <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
