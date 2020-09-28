2cc9f6fa604fbbf4848afe5bd3bda6b1ad9d98e6
Ronnie Feng <ronnie@ronniefeng.com>
2020-09-27 23:01:48 -0400

    initializing shopping  cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component.jsx";
 import { auth } from "../../firebase/firebase.utils.js";
 import {ReactComponent as Logo} from "../../assets/crown.svg";
 
@@ -19,6 +20,7 @@ const Header = ({currentUser}) => {
                 :
                 <Link className="option" to="/signin" >SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
     </div>
 }
