e8d9cb770b1f2087af7bc2d68397578c97e8fc82
Ronnie Feng <ronnie@ronniefeng.com>
2020-12-13 10:11:34 -0500

    refactoring shop data into an object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import "./collection.styles.scss";
-import {connect} from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item.component.jsx";
-import {selectCollection} from "../../redux/shop/shop.selectors.js";
+import React from 'react';
+import { connect } from 'react-redux';
 
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({match}) => {
-    return <div className="collection-page">
-        <h2>CATEGORY PAGE</h2>
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-}
+  );
+};
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        collection: selectCollection(ownProps.match.params.collectionId)(state)
-    }
-}
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
 export default connect(mapStateToProps)(CollectionPage);
