3c3a75100449a900852b37d1964ae055a7fae044
Ronnie Feng <ronnie@ronniefeng.com>
2020-12-14 16:53:56 -0500

    making adjustments on collection preview, now routing to the corresponding page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component.jsx";
+import {useHistory} from "react-router-dom";
 
 const CollectionPreview =({title, items}) =>{
+    const history = useHistory();
+    console.log(history)
     return <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
+        <h1 className="title" onClick={()=>{history.push(`shop/${title.toLowerCase()}`)}} >{title.toUpperCase()}</h1>
         <div className="preview">
             {items
                 .filter((item, index) => index < 4)
